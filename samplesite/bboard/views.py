3b1f072482da49624e3885e441363ceb03481ab8
ivoshet <ivan.v.shet@gmail.com>
2020-11-04 03:00:18 +0500

    added templates and static css

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,3 +1,9 @@
+from django.http import request
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Bb
+from django.template import loader
 
-# Create your views here.
+def index(request):
+    bbs = Bb.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {'bbs':bbs})
