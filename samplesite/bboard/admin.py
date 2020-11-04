8817b934f8115ccbd82b35d7294053193c0402d1
ivoshet <ivan.v.shet@gmail.com>
2020-11-04 23:00:10 +0500

    added rubric table

--- samplesite/bboard/admin.py
+++ samplesite/bboard/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Bb
+from .models import Rubric
 
-# Register your models here.
+
+class BbAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content')
+
+
+admin.site.register(Bb)
+admin.site.register(Rubric)
--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 
 class Bb(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField(null=True, blank=True)
-    price = models.FloatField(null=True, blank=True)
-    published = models.DateTimeField(auto_now_add=True, db_index=True)
+    title = models.CharField(max_length=50, verbose_name='goods')
+    content = models.TextField(null=True, blank=True, verbose_name='description')
+    price = models.FloatField(null=True, blank=True, verbose_name='price')
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='published')
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Rubrica')
+
+    class Meta:
+        verbose_name_plural = 'annoncement'
+        verbose_name = 'annoncement'
+        ordering = ['-published']
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Naming')
+
+    class Meta:
+        verbose_name_plural = 'rubrics'
+        verbose_name = 'rubric'
+        ordering = ['name']
